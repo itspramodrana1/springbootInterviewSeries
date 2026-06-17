697f49cea8258b76aa54a63ab25b4e37b400826f
itspramodrana1 <ranapramod663@gmail.com>
2026-06-17 18:10:31 +0530

    Day 2 Revised REST Controller VS Controller

--- src/main/java/com/thePramodCoder/interview_practice_service3/InterviewPracticeService3Application.java
+++ src/main/java/com/thePramodCoder/interview_practice_service3/InterviewPracticeService3Application.java
@@ -2,6 +2,10 @@ package com.thePramodCoder.interview_practice_service3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.webmvc.autoconfigure.WebMvcProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 @SpringBootApplication
  public  class InterviewPracticeService3Application {
@@ -10,4 +14,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 		SpringApplication.run(InterviewPracticeService3Application.class, args);
 	}
 
+    @Bean("JsonViewTemplate")
+	public View name(){
+		return new MappingJackson2JsonView();
+	}
 }
---
+++ src/main/java/com/thePramodCoder/interview_practice_service3/RestControllerVsController/DevController.java
@@ -0,0 +1,26 @@
+package com.thePramodCoder.interview_practice_service3.RestControllerVsController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class DevController {
+
+//    @GetMapping("/name")
+//    public String getName(){
+//        return "Pramod";
+//    }
+
+    public ModelAndView getNames(){
+        Map<String, Object> map = new HashMap<>();
+        map.put("Name", "Pramod");
+
+        return new ModelAndView("JsonViewTemplate", map);
+    }
+
+}
