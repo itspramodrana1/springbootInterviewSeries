cf7eb8059dffe623584e2417510c86790b4ecd42
itspramodrana1 <ranapramod663@gmail.com>
2026-07-29 19:12:21 +0530

    Revised Dispatcher Servalet II

--- src/main/java/com/thePramodCoder/interview_practice_service3/DispatcherServaletII/Controller.java
+++ src/main/java/com/thePramodCoder/interview_practice_service3/DispatcherServaletII/Controller.java
@@ -1,23 +1,23 @@
-package com.thePramodCoder.interview_practice_service3.DispatcherServaletII;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.Locale;
-
-@RestController
-public class Controller {
-
-    @PostMapping("files/upload")
-    public void uploadFile(@RequestParam("file") MultipartFile resume){
-      // Code to process the resume and email it to HRs.
-    }
-
-    @GetMapping("/info")
-    public void getLocalInfo(Locale locale){
-        System.out.println("Language : " + locale.getLanguage() + ", Region" + locale);
-    }
-}
+//package com.thePramodCoder.interview_practice_service3.DispatcherServaletII;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import java.util.Locale;
+//
+//@RestController
+//public class Controller {
+//
+//    @PostMapping("files/upload")
+//    public void uploadFile(@RequestParam("file") MultipartFile resume){
+//      // Code to process the resume and email it to HRs.
+//    }
+//
+//    @GetMapping("/info")
+//    public void getLocalInfo(Locale locale){
+//        System.out.println("Language : " + locale.getLanguage() + ",Region " + locale.getCountry());
+//    }
+//}
---
+++ src/main/java/com/thePramodCoder/interview_practice_service3/DispatcherServaletII/LocaleConfig.java
@@ -0,0 +1,20 @@
+//package com.thePramodCoder.interview_practice_service3.DispatcherServaletII;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.LocaleResolver;
+//import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+//
+//import java.util.Locale;
+//
+//@Configuration
+//public class LocaleConfig {
+//
+//    @Bean
+//    public LocaleResolver localeResolver(){
+//        AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+//        // Default locale : French (France)
+//        localeResolver.setDefaultLocale(new Locale("fr", "FR"));
+//        return localeResolver;
+//    }
+//}
